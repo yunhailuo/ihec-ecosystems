c46e00f687fd72130c0b8fd4d817f1f182f5c847
sitag <sgakkhar@bcgsc.ca>
2018-10-17 22:02:28 -0700

    add __init__ for using as library

---
+++ version_metadata/validator.py
@@ -0,0 +1,51 @@
+from config import Config
+from utils import cmn, json2, logger
+
+import validate_sample
+import validate_experiment
+
+
+
+def main(args):
+	status = dict()
+
+	if not args.has('-config'):
+		args.add_key('-config',  "./config.json")
+
+	logger(str([args.keys, args.args()]) + '\n')
+	
+	if not args.has('-out'):
+		logger('#__noOutFileGiven___\n')
+		return
+
+	if cmn.fexists(args['-out']) and not args.has('-overwrite-outfile'):
+		logger('#__outfile:{0} exists\n'.format(args['-out']))
+		return
+
+	#try:
+	if True:
+		if args.has('-extract'):
+			import sraparse
+			return sraparse.SRAParseObjSet.extract_attributes_to_json(args.args())
+		elif args.has("-test-sample"):
+			testargs = ["./examples/samples.xml", "-config:{0}".format(args['-config']), "-out:./examples/samples.versioned.xml"]
+			status['test'] = validate_sample.main(Config(testargs))
+		elif args.has("-sample"):
+			status['sample'] = validate_sample.main(args)
+		elif args.has("-experiment"):
+			status['experiment'] = validate_experiment.main(args) 
+		else:
+			raise NotImplementedError("#__unknownArguments__")
+	else:
+	#except Exception as err:
+		logger('#__unexpected__\n')
+		logger(str(err.message) + '\n')
+	
+	logger("#__finished__\n")
+	return status
+
+
+if __name__ == '__main__':
+	main(Config.sys())
+
+
